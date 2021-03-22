f91d067e30ce1b6a95c72b5606d5bb81af577f23
namikawa07 <tatsuki111407@gmail.com>
2021-03-22 23:02:25 +0900

    Active_storageの導入、user情報の編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get    '/login', to: "sessions#new"
   post   '/login',   to: "sessions#create"
   delete '/logout',  to: "sessions#destroy"
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :update]
   resources :sessions, only: [:new, :create, :destroy]
 end
