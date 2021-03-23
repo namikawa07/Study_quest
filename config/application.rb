101152ee36a7fdef8e1ccbf0f44b8f411a7b56af
namikawa07 <tatsuki111407@gmail.com>
2021-03-23 12:09:45 +0900

    i18nで日本語対応

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,12 @@ Bundler.require(*Rails.groups)
 
 module StudyQuest
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
