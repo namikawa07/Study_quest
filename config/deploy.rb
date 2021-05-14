69340df44b74c96d48bae464b539b4ce6ad317ae
namikawa07 <tatsuki111407@gmail.com>
2021-05-14 16:25:34 +0900

    masterブランチをmainブランチに変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :deploy_to, '/var/www/Study_quest'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:namikawa07/Study_quest.git'
 
+set :branch, "main"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
