c28f061ff72aee6a7279cd1b1be782649b7e67f7
namikawa07 <tatsuki111407@gmail.com>
2021-05-16 20:36:25 +0900

    environment.jsのvue-loaderの削除

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,2 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
 module.exports = environment
