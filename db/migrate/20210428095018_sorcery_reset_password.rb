8e7a61d93879f16fdc7ad4fee4bcaa55d7bca5fa
namikawa07 <tatsuki111407@gmail.com>
2021-05-16 21:51:18 +0900

    reset_password_tokenの作成

--- db/migrate/20210428095018_sorcery_reset_password.rb
+++ db/migrate/20210428095018_sorcery_reset_password.rb
@@ -1,5 +1,7 @@
 class SorceryResetPassword < ActiveRecord::Migration[5.2]
   def change
-
+    add_column :users, :reset_password_token, :string, default: nil
+    add_column :users, :reset_password_token_expires_at, :datetime, default: nil
+    add_column :users, :reset_password_email_sent_at, :datetime, default: nil
   end
 end
