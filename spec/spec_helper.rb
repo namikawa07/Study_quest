26d1353ff5bd20b73ca45dbe3d7bf07fdda4950d
namikawa07 <tatsuki111407@gmail.com>
2021-05-18 23:07:26 +0900

    circleCIでrspecが通るようにする

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@
 require 'capybara/rspec'
 RSpec.configure do |config|
   config.filter_run_when_matching :focus
+  config.full_backtrace = true
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
