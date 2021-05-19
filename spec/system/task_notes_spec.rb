7e70bae677ae2e36af07abb793b54b4b7b149e1b
namikawa07 <tatsuki111407@gmail.com>
2021-05-19 20:51:59 +0900

    rspec

--- spec/system/task_notes_spec.rb
+++ spec/system/task_notes_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe 'タスクノート機能', type: :system, js: true  do
     end
     describe 'タスクノート作成' do
       context '記入値が正常な場合' do
-        it 'ノートの作成が成功する' do
+        fit 'ノートの作成が成功する' do
           click_on('ノートを作成する')
           expect(page).to have_content('Note title')
           fill_in 'note[title]', with: 'test_task_note'
@@ -40,12 +40,11 @@ RSpec.describe 'タスクノート機能', type: :system, js: true  do
         end
       end
       context '記入値が未記入' do
-        it 'エラーが表示される' do
+        fit 'エラーが表示される' do
           click_on('ノートを作成する')
           expect(page).to have_content('Note title')
           fill_in 'note[body]', with: 'test_task_note_body'
           click_on('作成')
-          sleep 1
           expect(page).to have_content('タイトルを入力して下さい')
         end
       end
