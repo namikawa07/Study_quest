fe51e03652daa0adb154874f6b48a5cb7a53157b
namikawa07 <tatsuki111407@gmail.com>
2021-05-22 07:03:26 +0900

    メール形式の変更

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find user.id
     @url = edit_password_reset_url(@user.reset_password_token)
-    mail(to: user.email, subject: 'パスワード再設定用メール')
+    mail(to: user.email, subject: 'パスワード再設定用メール') do |format|
+      format.html #HTMLメールを指定
+    end
   end
 end
