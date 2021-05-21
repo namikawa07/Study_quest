29b1efd355c1b09c7e51cd4743041b682ae8c49c
namikawa07 <tatsuki111407@gmail.com>
2021-05-22 06:12:54 +0900

    メール送信元設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: 'namikawait@gmail.com', from: @contact.email, subject: 'お問い合わせ'
+    mail to: 'namikawait@gmail.com', from: 'namikawa.tatsuki@gmail.com', subject: 'お問い合わせ'
   end
 end
