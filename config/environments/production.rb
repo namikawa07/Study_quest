0729c103a47a7a820c489429b611791cbab62a0e
namikawa07 <tatsuki111407@gmail.com>
2021-05-22 03:34:26 +0900

    S3の設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
-
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
