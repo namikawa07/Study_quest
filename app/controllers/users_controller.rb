bff291ab771db7149bd12293a2484bb47f5f2b28
namikawa07 <tatsuki111407@gmail.com>
2021-05-21 19:38:56 +0900

    iconの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,12 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
     respond_to do |format|
       if @user.update(user_params)
+        if @user.icon.present?
+          @user.icon = "#{@user.id}.jpg"
+          image_path = Rails.root.join("#{@user.icon}")
+          @user.icon.attach(io: File.open(image_path), filename: "#{@user.icon}")
+          @user.save!
+        end
         flash[:success] = t('users.update.Success')
         format.html { redirect_to users_path }
         format.js { render js: "window.location = '#{users_path}'" }
