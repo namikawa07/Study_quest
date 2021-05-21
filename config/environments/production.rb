361af18085f4b2063c0a5b8fc56498e7255305fa
namikawa07 <tatsuki111407@gmail.com>
2021-05-22 05:37:59 +0900

    メール設定の変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: namikawait@gmail.com,
-    password: appletatsuki07,
+    user_name: 'namikawait@gmail.com',
+    password: 'appletatsuki07',
     authentication: :plain,
     enable_starttls_auto: true
   }
