4eba18e6f110678243abdb836d062aa5b5072cd9
namikawa07 <tatsuki111407@gmail.com>
2021-05-22 05:36:01 +0900

    メール送信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   mail = ENV['MAIL_ADDRESS']
@@ -12,8 +12,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: mail,
-    password: pass,
+    user_name: namikawait@gmail.com,
+    password: appletatsuki07,
     authentication: :plain,
     enable_starttls_auto: true
   }
