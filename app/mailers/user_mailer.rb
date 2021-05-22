f289a46f341a05cdd30bc870a64f46fe70a2ad28
namikawa07 <tatsuki111407@gmail.com>
2021-05-22 17:50:57 +0900

    rubocop

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,6 @@ class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find user.id
     @url = edit_password_reset_url(@user.reset_password_token)
-    mail(to: user.email, subject: 'パスワード再設定用メール') do |format|
-      format.html #HTMLメールを指定
-    end
+    mail(to: user.email, subject: 'パスワード再設定用メール', &:html)
   end
 end
