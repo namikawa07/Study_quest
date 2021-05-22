33b59ac4a12251f30861fbd2e267a1dd15cac9d1
namikawa07 <tatsuki111407@gmail.com>
2021-05-22 17:17:58 +0900

    メールエラーの改善

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find user.id
-    @url = "https://taskquest-app.com" + edit_password_reset_path(@user.reset_password_token)
+    @url = edit_password_reset_url(@user.reset_password_token)
     mail(to: user.email, subject: 'パスワード再設定用メール') do |format|
       format.html #HTMLメールを指定
     end
